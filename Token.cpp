3b6613077fd38a657f75ce63f719ee273b93280d
Thibaut Chavet <email@thibautchavet.net>
2017-03-01 17:07:54 +0100

    Add integer conversion

--- Token.cpp
+++ Token.cpp
@@ -7,12 +7,27 @@ Token::Token(int tokenLine, int tokenCol, std::string tokenType, std::string tok
 	col = tokenCol;
 	type = tokenType;
 	value = tokenValue;
+	if(tokenValue == "")
+		valueType = Null;
+	else
+		valueType = String;
+}
+
+Token::Token(int tokenLine, int tokenCol, std::string tokenType, int tokenValue)
+{
+	line = tokenLine;
+	col = tokenCol;
+	type = tokenType;
+	intValue = tokenValue;
+	valueType = Int;
 }
 
 void Token::print()
 {
 	std::cout<<line<<","<<col<<","<<type;
-	if(value != "")
+	if(valueType == Int)
+		std::cout<<","<<intValue;
+	else if(valueType == String)
 		std::cout<<","<<value;
 	std::cout<<std::endl;
 }
--- Token.hpp
+++ Token.hpp
@@ -3,11 +3,15 @@
 class Token
 {
 public:
+	enum Type {Int, String, Null};
 	Token(int tokenLine, int tokenCol, std::string tokenType, std::string tokenValue="");
+	Token(int tokenLine, int tokenCol, std::string tokenType, int tokenValue);
 	void print();
 
 protected:
 	int line, col;
 	std::string type;
 	std::string value;
+	int intValue;
+	Type valueType;
 };
